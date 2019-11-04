faec7d4926bad3bc5c5ca724cf394b4787776968
IulianStave <iulian.staverescu@gmail.com>
2019-11-04 18:27:35 +0200

    Clean code

--- stocks.py
+++ stocks.py
@@ -1,7 +1,9 @@
 from pandas_datareader import data
 from bokeh.plotting import figure, show, output_file
+from bokeh.embed import components
 from bokeh.models.annotations import Title
 from datetime import datetime as dt
+from bokeh.resources import CDN
 
 def inc_dec(open, close):
     if open < close:
@@ -13,8 +15,8 @@ def inc_dec(open, close):
 #start_date = dt(2019,10,1)
 #end_date = dt(2019,10,10) 
 
-start_date = dt(2016,3,1)
-end_date = dt(2016,3,10) 
+start_date = dt(2019,3,1)
+end_date = dt(2019,5,10) 
 
 hours_12_in_ms = 12*60*60*1000
 
@@ -27,11 +29,18 @@ chart.title.text = "Chart stocks"
 df["Status"] = [inc_dec(open, close) for open, close in zip(df.Open, df.Close)]
 df["Middle"] = (df.Open + df.Close)/2
 df["Height"] = abs(df.Close - df.Open)
+chart.grid.grid_line_alpha = 0.3
 
-chart.rect(df.index[df.Status == 'Increase'], df.Middle[df.Status == 'Increase'], hours_12_in_ms, df.Height[df.Status == 'Increase'], fill_color = "green", line_color = "black")
+chart.segment(df.index, df.High, df.index, df.Low, color = "Black")
+chart.rect(df.index[df.Status == 'Increase'], df.Middle[df.Status == 'Increase'], hours_12_in_ms, df.Height[df.Status == 'Increase'], fill_color = "#CCFFFF", line_color = "black")
 
-chart.rect(df.index[df.Status == 'Decrease'], df.Middle[df.Status == 'Decrease'], hours_12_in_ms, df.Height[df.Status == 'Decrease'], fill_color = "red")
+chart.rect(df.index[df.Status == 'Decrease'], df.Middle[df.Status == 'Decrease'], hours_12_in_ms, df.Height[df.Status == 'Decrease'], fill_color = "#FF3333", line_color = "gray")
 
+script1js, div1 = components(chart)
+print(script1js)
+print(div1)
 
-output_file('stocks.html')
-show(chart)
+cdn_js1 = CDN.js_files[0]
+cdn_css1 = CDN.css_files[0]
+#output_file('stocks.html')
+#show(chart)
